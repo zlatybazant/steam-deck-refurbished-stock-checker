50aefbeb29f6c3ec40ac5cf2d3adeae49ee86cde
zlatybazant <52216373+zlatybazant@users.noreply.github.com>
2025-01-17 12:45:26 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /path/to/steam-deck-refurbished-stock-checker/deck_venv/bin/activate
-python3 /path/to/devel/steam-deck-refurbished-stock-checker/checker.py
+python3 /path/to/steam-deck-refurbished-stock-checker/checker.py
